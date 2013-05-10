13cb8c1ee49716e76eadc070440dfe4e7fd77d18
ssttevee <stevyo99@yahoo.ca>
2013-05-10 12:42:44 -0700

    resets payload after verification and added javadoc (for no reason)

--- src/com/ssttevee/cloudapp/CAApplication.java
+++ src/com/ssttevee/cloudapp/CAApplication.java
@@ -15,7 +15,19 @@ public class CAApplication extends Application {
 	public CAApplication() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	/**
+	 * Checks if the payload in the given purchase is the same as 
+	 * the payload given, then resets the payload back to nothing
+	 * 
+	 * @param p
+	 * 			the purchase containing the payload to compare
+     * @return {@code true} if the payloads are the same,
+     *         {@code false} otherwise.
+	 */
     public boolean verifyDeveloperPayload(Purchase p) {
+    	String payload = this.payload;
+    	this.payload = "";
         return payload.equals(p.getDeveloperPayload());
     }
 
